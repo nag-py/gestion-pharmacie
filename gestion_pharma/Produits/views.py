8457e1670c0cad5134a210996c087d79ea9fac80
nag-py <captaintn777@gmail.com>
2024-10-20 15:46:53 +0200

    afficher les donnees bdd

--- gestion_pharma/Produits/views.py
+++ gestion_pharma/Produits/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    #recuperer els valeurs de la bdd
+    donnees = Produit.objects.all()
+
+    context = {
+        'donnees': donnees,
+    }
+    return render(request, 'home.html', context)
